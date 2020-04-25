2731cd52a19027fb682433f90eecfb838e1f419d
Akis Mparekas <thunderlake@gmail.com>
2020-04-25 12:46:08 +0300

    Added Auto-Embed Support

--- swiss-army-knife/php/functions.php
+++ swiss-army-knife/php/functions.php
@@ -1,15 +1,143 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
+
+//This function converts url links to embed codes
+function url2embed( $post ) 
+{
+	
+	$gl_video_width = 604;
+	$gl_video_height = 505;
+			
+	preg_match_all('/<a href=\"(.*)\">(.*)<\/a>/', $post, $out);
+	
+	$links = $out['1'];
+		
+	foreach ($links as $link) 
+	{
+		$start = 0;
+		$end = 0;
+				
+		if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[\w\-?&!#=,;]+/[\w\-?&!#=/,;]+/|(?:v|e(?:mbed)?)/|[\w\-?&!#=,;]*[?&]v=)|youtu\.be/)([\w-]{11})(?:[^\w-]|\Z)%i', $link, $output)) 
+		{
+				
+			$yt_ID = $output['1'];
+			$yt_URL = 'https://' . $output['0'];
+				
+			if (preg_match("/start=([0-9]+)/", $link, $un))
+				$start = $un['1'];
+				
+			if (preg_match("/end=([0-9]+)/", $link, $un))
+				$end = $un['1'];
+				
+			$wto = '<iframe class="youtube-player" src="https://www.youtube.com/embed/' . $yt_ID . '?version=3&amp;rel=1&amp;fs=1&amp;autohide=2&amp;showsearch=0&amp;showinfo=1&amp;iv_load_policy=1';
+					
+			if (!empty($start))
+				$wto .= '&amp;start=' . $start;
+				
+			if (!empty($end))
+				$wto .= '&amp;end=' . $end;
+				
+			$wto .= '&amp;wmode=transparent" allowfullscreen="true" allow="autoplay; encrypted-media" width="100%" height="404" style="border: 0px none; display: block; margin: 0px;"></iframe>';
+				
+			$post = str_replace('<a href="' . $link . '">' . $link . '</a>', $wto, $post);
+		}
+			
+		if (preg_match("/dailymotion/", $link)) 
+		{
+				
+			preg_match("/\/video\/(.+)/",$link, $matches);
+			preg_match("/video\/([^_]+)/", $link, $matche);
+				
+			$yt_id = (!empty($matche[1])) ? $matche[1] : $matches[1];
+				
+			$embed = '<div class="flex-video"><iframe frameborder="0" width="100%" height="400" src="//www.dailymotion.com/embed/video/' . $yt_id . '" allowfullscreen></iframe></div>';
+				
+			$post = str_replace('<a href="' . $link . '">' . $link . '</a>', $embed, $post);
+		}
+			
+		if ( preg_match('#https?://(player\.)?vimeo\.com(/video)?/(\d+)#i', $link, $matches) ) 
+		{
+							
+			$yt_id = trim($matches[3]);
+							
+			$embed = '<div class="flex-video"><iframe src="https://player.vimeo.com/video/' . $yt_id . '" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
+				
+			$post = str_replace('<a href="' . $link . '">' . $link . '</a>', $embed, $post);
+		}
+			
+		if (preg_match("/facebook.com\/(.*?)/", $link) || (preg_match('/facebook\.com/i', $link))) 
+		{
+			$embed = '<div class="flex-video"><iframe src="https://www.facebook.com/plugins/video.php?href=' . urlencode( $link ) . '&show_text=0&width=367" width="367" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></div>';
+				
+			$post = str_replace('<a href="' . $link . '">' . $link . '</a>', $embed, $post);
+		}
+			
+		if ( preg_match('#^https?://twitter\.com/(?:\#!/)?(\w+)/status(es)?/(\d+)$#i', $link, $matches ) ) 
+		{
+								
+			$status_id = $matches[3];
+								
+			$uri = 'https://api.twitter.com/1/statuses/oembed.json?id=' . $status_id . '&omit_script=true';
+				
+			$source = @file_get_contents($uri, true); //getting the file content
+				
+			if ( ( $source !== false ) && ( !empty( $source ) ) ) {
+				
+				$decode = json_decode($source, true); //getting the file content as array
+				
+				$embed = $decode['html'] . '<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+				
+				unset( $source, $decode );
+				
+				$post = str_replace('<a href="' . $link . '">' . $link . '</a>', $embed, $post);
+			}
+				
+		}
+			
+	}
+		
+		
+	//Lazy Load for YouTube Only
+	$lazyIframe2 = '<iframe
+					  width="100%"
+					  height="' . $gl_video_height . '"
+					  src="$3"					  srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=\'$3\'><img src=\'$2\' alt=\'Youtube Video\'><span>▶</span></a>"
+					  frameborder="0"
+					  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					  allowfullscreen
+					  title="Youtube Video"
+					></iframe>';
+		
+	//Maybe the post doesn't has any URL links. For now, search only for youtube URLs
+	$post = preg_replace(
+			"/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
+			"$lazyIframe",
+			$post );
+			
+	//Convert youtube embed code from Blogger
+	$pat = '/(<div class=\"separator\" style=\"clear: both; text-align: center;\">)?<iframe.+data-thumbnail-src=[\'"]([^\'"]+)[\'"].+src=[\'"]([^\'"]+)[\'"].*><\/iframe>(<\/div>)?/';
+		
+	$post = preg_replace($pat, $lazyIframe2, $post );
+		
+	return $post;
+}
+
 //
 //
 //The functions below taken from WordPress
 //
 //
 //
-	function _autop_newline_preservation_helper( $matches ) {
-		return str_replace( "\n", "<WPPreserveNewline />", $matches[0] );
+	if ( !function_exists( '_autop_newline_preservation_helper' ) )
+	{
+		function _autop_newline_preservation_helper( $matches ) {
+			return str_replace( "\n", "<WPPreserveNewline />", $matches[0] );
+		}
 	}
-
-	function get_html_split_regex() {
+	
+if ( !function_exists( 'get_html_split_regex' ) )
+{
+	function get_html_split_regex() 
+	{
 		static $regex;
 	 
 		if ( ! isset( $regex ) ) {
@@ -55,11 +183,16 @@
 	 
 		return $regex;
 	}
+}
 
+if ( !function_exists( 'wp_html_split' ) )
+{
 	function wp_html_split( $input ) {
 		return preg_split( get_html_split_regex(), $input, -1, PREG_SPLIT_DELIM_CAPTURE );
 	}
-
+}
+if ( !function_exists( 'wp_replace_in_html_tags' ) )
+{
 	function wp_replace_in_html_tags( $haystack, $replace_pairs ) {
 		// Find all elements.
 		$textarr = wp_html_split( $haystack );
@@ -100,7 +233,10 @@
 	 
 		return $haystack;
 	}
+}
 
+if ( !function_exists( 'wpautop' ) )
+{
 	function wpautop( $pee, $br = true ) {
 		$pre_tags = array();
 
@@ -256,3 +392,4 @@
 
 		return $pee;
 	}
+}
